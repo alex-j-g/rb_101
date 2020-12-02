12fbbfcec3402f694cbd5e6ee7efa72f8896dbdd
Alex <golstonaj@gmail.com>
2020-12-01 16:35:32 -0800

    added loan calculator w/ yml file

---
+++ lesson_2/loan_calculator.rb
@@ -0,0 +1,111 @@
+require 'yaml'
+MESSAGES = YAML.load_file('loan_messages.yml')
+
+def clear_screen
+  system('clear') || system('cls')
+end
+
+def prompt(message)
+  puts("=> #{message}")
+end
+
+def valid_user?(name)
+  name.empty? || name.to_i.to_s == name
+end
+
+def valid_number?(num)
+  num.to_i.to_s == num && num.to_i > 0
+end
+
+def valid_apr?(num)
+  num.to_f.to_s == num && num.to_f > 0 || num.to_i.to_s == num && num.to_i > 0
+end
+
+def years_to_months(num)
+  num.to_i * 12
+end
+
+def interest_monthly(num)
+  (num / 100) / 12
+end
+
+prompt(MESSAGES['welcome'])
+sleep(0.5)
+
+user = ''
+loop do
+  prompt(MESSAGES['name'])
+  user = gets.chomp
+
+  if valid_user?(user)
+    prompt(MESSAGES['invalid_name'])
+    sleep(0.5)
+  else
+    break
+  end
+end
+
+clear_screen
+user.capitalize!
+
+loan = ''
+apr = ''
+month_interest = ''
+duration = ''
+duration_month = ''
+month_payment = ''
+
+loop do
+  loop do
+    prompt("#{user}, " + MESSAGES['loan_amount'])
+    loan = gets.chomp
+    if valid_number?(loan)
+      clear_screen
+      break
+    else
+      prompt(MESSAGES['invalid_amount'])
+    end
+    sleep(0.5)
+  end
+
+  loop do
+    prompt(MESSAGES['loan_apr'])
+    apr = gets.chomp
+    if valid_apr?(apr)
+      clear_screen
+      break
+    else
+      prompt(MESSAGES['invalid_apr'])
+      sleep(0.5)
+    end
+  end
+
+  month_interest = interest_monthly(apr.to_f)
+
+  loop do
+    prompt(MESSAGES['loan_duration'])
+    duration = gets.chomp
+    if valid_number?(duration)
+      clear_screen
+      break
+    else
+      prompt(MESSAGES['invalid_duration'])
+      sleep(0.5)
+    end
+  end
+
+  duration_month = years_to_months(duration)
+
+  month_payment = loan.to_i * (month_interest / (1 - (1 + month_interest)**(-duration_month)))
+
+  prompt("Yor loan amount is: #{loan}")
+  prompt("Your APR is: #{apr}")
+  prompt("Your loan duration is: #{duration} years")
+  prompt("If correct, 'Y' to continue.")
+  answer = gets.chomp
+  break if answer.downcase.start_with? 'y'
+  clear_screen
+end
+
+prompt("#{user} your monthly payment is: $#{month_payment.round(2)}")
+prompt(MESSAGES['goodbye'])
